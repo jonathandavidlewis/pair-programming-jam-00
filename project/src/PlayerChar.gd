54d22417df8e68edc7f299b0e3e4c955aa6d2862
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2023-02-01 19:10:39 -0800

    Add acceleration and collision to main character

--- project/src/PlayerChar.gd
+++ project/src/PlayerChar.gd
@@ -1,17 +1,20 @@
 extends KinematicBody2D
 
-export(int) var MAX_VELOCITY = 1000 
+export(int) var MAX_VELOCITY = 10000 
+export(int) var ACCELERATION = 5
 
 var velocity = Vector2()
+
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	velocity = get_input()
+	var target_velocity = get_input_direction() * MAX_VELOCITY
+	velocity = velocity.linear_interpolate(target_velocity, ACCELERATION * delta)
 	move_and_slide(velocity * delta)
 
-func get_input() -> Vector2:
+func get_input_direction() -> Vector2:
 	var input_direction = Vector2(0, 0)
 	
 	if Input.is_action_pressed("ui_left"):
@@ -22,6 +25,7 @@ func get_input() -> Vector2:
 		input_direction.y -= 1
 	if Input.is_action_pressed("ui_down"):
 		input_direction.y += 1
-	print("get input")	
-	
-	return input_direction.normalized() * MAX_VELOCITY
+
+	return input_direction.normalized()
+
+
