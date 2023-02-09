8f0a9aafa05ab974f8573d42b2514affde11ac42
loteque <andrewandroid@gmail.com>
2023-02-09 11:51:26 +0100

    add immunity to holes durring dash, some refactoring.

--- project/src/PlayerChar.gd
+++ project/src/PlayerChar.gd
@@ -2,37 +2,43 @@ extends KinematicBody2D
 
 export(int) var MAX_VELOCITY := 100
 export(float) var ACCELERATION := 3.0
-export(float) var DASH_MULTIPLIER := 12
+export(float) var DASH_MULTIPLIER := 12.0
 export(int) var DASH_ACCEL_MULT := 30
 export(float) var DASH_WAIT := 1.0
+export(float) var DASH_DURRATION := 0.3
 
 var velocity := Vector2()
 var can_move := true
 var dash_timeout := false
+var dashing := false
 
+onready var DEATH_HIT_BOX = $Area2D/CollisionPolygon2D
 onready var DEATH_SOUND = $Sound/DeathSound
 
 func _physics_process(delta: float) -> void:
-	
+	var target_velocity = get_input_direction() * MAX_VELOCITY
+
 	if !can_move:
 		return
 	
-	var target_velocity = get_input_direction() * MAX_VELOCITY
-	set_velocity(target_velocity, ACCELERATION, delta)
-	get_input_abilities(delta)
+	if get_input_abilities() == "dash" and can_dash():
+		DEATH_HIT_BOX.disabled = true
+		dash(target_velocity * DASH_MULTIPLIER, ACCELERATION * DASH_ACCEL_MULT, delta)
+
+	if !is_dashing():
+		DEATH_HIT_BOX.disabled = false
+		set_velocity(target_velocity, ACCELERATION, delta)
+
 	velocity = move_and_slide(velocity)
 
 func _on_Area2D_area_entered(area:Area2D) -> void:
-	if area.is_in_group("Hole"):
+	if area.is_in_group("Death"):
 		die()
 
-func get_input_abilities(delta) -> void:
-	if Input.is_action_just_pressed("ui_accept") and can_dash():
-		dash(delta)
-
-func dash(delta):
-	set_dash_velocity(get_input_direction() * MAX_VELOCITY, ACCELERATION, delta)
-	disable_dashing()
+func get_input_abilities() -> String:
+	if Input.is_action_just_pressed("ui_accept"):
+		return "dash"
+	return "none"
 
 func get_input_direction() -> Vector2:
 	var input_direction = Vector2(0, 0)
@@ -51,23 +57,43 @@ func get_input_direction() -> Vector2:
 func can_dash() -> bool:
 	return !dash_timeout
 
-func set_dash_velocity(target_velocity, acceleration, delta) -> void:
-	set_velocity(target_velocity * DASH_MULTIPLIER, acceleration * DASH_ACCEL_MULT, delta)
-	
-func disable_dashing():
+func is_dashing() -> bool:
+	return dashing
+
+func dash(target_velocity, acceleration, delta) -> void:
+	set_dashing(true)
+	set_velocity(target_velocity, acceleration, delta)
+	disable_dash()
+	set_dash_durration(DASH_DURRATION, "set_dashing", [false])
+
+func set_dashing(state: bool) -> void:
+	if state == true: 
+		dashing = true
+	else:
+		dashing = false
+
+func disable_dash() -> void:
 	dash_timeout = true
-	set_dash_timeout()
-	
-func enable_dashing():
-	dash_timeout = false
-	
-func set_dash_timeout():
+	set_dash_timeout(DASH_WAIT)
+
+func set_dash_durration(time: float, call_back: String, call_back_arg: Array) -> void:
+	var durration_timer = Timer.new()
+	durration_timer.wait_time = time
+	durration_timer.connect("timeout", self, call_back, call_back_arg)
+	durration_timer.one_shot = true
+	add_child(durration_timer)
+	durration_timer.start()
+
+func set_dash_timeout(time: float) -> void:
 	var dash_timer = Timer.new()
-	dash_timer.wait_time = DASH_WAIT
+	dash_timer.wait_time = time
 	dash_timer.connect("timeout", self, "enable_dashing")
 	dash_timer.one_shot = true
 	add_child(dash_timer)
 	dash_timer.start()
+
+func enable_dashing() -> void:
+	dash_timeout = false
 
 func set_velocity(target_velocity, acceleration, delta) -> void:
 	velocity = velocity.linear_interpolate(target_velocity, acceleration * delta)
