59197ff0feaf394dc8fe930a1a61c630e9fa1211
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2023-02-14 17:19:46 -0800

    Add sound to slot machine

--- project/src/StartMenu.gd
+++ project/src/StartMenu.gd
@@ -1,5 +1,14 @@
 extends Node
 
+onready var start_button = $Button
+
+func _ready():
+	start_button.grab_focus()
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		_on_Start_pressed("start")
+
 func _on_Start_pressed(extra_arg_0:String) -> void:
 	Signals.emit_signal("button_pressed", extra_arg_0)
 	get_node("Button").visible = false
